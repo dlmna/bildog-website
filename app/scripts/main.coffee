9a71c901d8ed9a1932abe1d9f91fc7d68558436f
Daniel Mania <daniel@klarblick.org>
2014-09-26 17:46:43 +0200

    feature: open content via navigation

--- app/scripts/main.coffee
+++ app/scripts/main.coffee
@@ -1,2 +1,9 @@
+window.UI = {}
+
+UI.openTab = (element, template) ->
+  $(".nav li.active").removeClass "active"
+  $(element).parent().addClass "active"
+  Helpers.loadContent template
+
 $ ->
   Helpers.loadContent "/templates/home.html"
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,12 @@
 (function() {
+  window.UI = {};
+
+  UI.openTab = function(element, template) {
+    $(".nav li.active").removeClass("active");
+    $(element).parent().addClass("active");
+    return Helpers.loadContent(template);
+  };
+
   $(function() {
     return Helpers.loadContent("/templates/home.html");
   });
